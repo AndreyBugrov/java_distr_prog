0853008fee463997f551e3c9d499658a6315d334
bugrov.and <bugrovandriy@yandex.ru>
2023-11-21 10:18:29 +0300

    new turn

--- lab1/src/main/java/org/example/Game.java
+++ lab1/src/main/java/org/example/Game.java
@@ -1,16 +1,121 @@
 package org.example;
 
 public class Game implements GameInterface{
-    int boardSize = 19;
+    final int boardSize = 19;
+    final int win_num = 6;
     int players = 0;
     int turn = 1;
-    public boolean win_for_row(){return false;}
-    public boolean win_for_column(){return false;}
-    public boolean win_for_diag(){return false;}
+    int[][] board = new int[boardSize][boardSize];
 
-    public boolean win_check(){
-        return win_for_column() || win_for_row() || win_for_diag();
+
+    public boolean win_for_row(){
+        int counter=0;
+        for(int i=0;i<boardSize;i++){
+            for (int j=0;j<boardSize;j++){
+                if(board[i][j]==1){
+                    counter++;
+                    if(counter == win_num) {
+                        return true;
+                    }
+                }else{
+                    counter=0;
+                }
+            }
+        }
+        return false;}
+    public boolean win_for_column(){
+        int counter=0;
+        for(int j=0;j<boardSize;j++){
+            for (int i=0;i<boardSize;i++){
+                if(board[i][j]==1){
+                    counter++;
+                    if(counter == win_num) {
+                        return true;
+                    }
+                }else{
+                    counter=0;
+                }
+            }
+        }
+        return false;}
+    public boolean win_for_main_diags(){
+        int base_i = 0, base_j = 0;
+        for(base_j=0;base_j<boardSize-1;base_j++){
+            int counter = 0;
+            for(int i=base_i,j=base_j;i<boardSize;i++,j++){
+                if(board[i][j]==1){
+                    counter++;
+                    if(counter == win_num) {
+                        return true;
+                    }
+                }else{
+                    counter=0;
+                }
+            }
+        }
+        base_j=0;
+        for(base_i=1;base_i<boardSize-1;base_i++){
+            int counter = 0;
+            for(int i=base_i,j=base_j;i<boardSize;i++,j++){
+                if(board[i][j]==1){
+                    counter++;
+                    if(counter == win_num) {
+                        return true;
+                    }
+                }else{
+                    counter=0;
+                }
+            }
+        }
+        return false;}
+    public boolean win_for_not_main_diags(){
+        int base_i = 1, base_j = 0;
+        for(base_i=1;base_i<boardSize;base_i++){
+            int counter = 0;
+            for(int i=base_i,j=base_j;i>=0;i--,j++){
+                if(board[i][j]==1){
+                    counter++;
+                    if(counter == win_num) {
+                        return true;
+                    }
+                }else{
+                    counter=0;
+                }
+            }
+        }
+        for(base_j=1;base_j<boardSize;base_j++){
+            int counter = 0;
+            for(int i=base_i,j=base_j;j>=0;i--,j++){
+                if(board[i][j]==1){
+                    counter++;
+                    if(counter == win_num) {
+                        return true;
+                    }
+                }else{
+                    counter=0;
+                }
+            }
+        }
+        return false;
     }
-    public void first_move(){}
-    public void move(){}
+    public boolean win_check(){
+        return win_for_column() || win_for_row() || win_for_main_diags() || win_for_not_main_diags();
+    }
+    public void first_move(){
+        int middle = boardSize/2;
+        board[middle][middle] = 1;
+    }
+    //TO DO: Input to gameInterface
+    public void move(){
+        //...
+        // get int x_1, int y_1, int x_2, int y_2 from user
+    }
+    public void process(){
+        first_move();
+        while(win_check()){
+            move();
+            turn++;
+            turn %= 2;
+        }
+    }
 }
--- lab1/src/main/java/org/example/GameInterface.java
+++ lab1/src/main/java/org/example/GameInterface.java
@@ -3,9 +3,10 @@ package org.example;
 public interface GameInterface {
     public boolean win_for_row();
     public boolean win_for_column();
-    public boolean win_for_diag();
+    public boolean win_for_main_diags();
+    public boolean win_for_not_main_diags();
 
     public boolean win_check();
     public void first_move();
-    public void move();
+    //public void move(int x_1, int y_1, int x_2, int y_2);
 }
